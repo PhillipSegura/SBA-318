a71a5014ea947e7c47db5bddc467c0b276087424
PhillipSegura <PhillipaSegura@gmail.com>
2024-12-01 10:32:36 -0700

    Finished adding needed files and folders. Downloaded express and ejs within package-lock.json and packagage.json

---
+++ data/comment.js
@@ -0,0 +1,5 @@
+const comments = [
+  { id: 1, author: "John Doe", content: "This is a great post!" },
+  { id: 2, author: "Jane Smith", content: "I agree with John." },
+  { id: 3, author: "Mike Johnson", content: "I disagree with John." },
+];
