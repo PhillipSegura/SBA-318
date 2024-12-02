68c3989be216838da801bc12c553dde27668e73d
PhillipSegura <PhillipaSegura@gmail.com>
2024-12-01 22:13:20 -0700

    Added and adjusted styling, post.js and routes .js. I'm not sure why the setting arent pushing or publishing the contentent but I intend on polishing it soon

---
+++ data/post.js
@@ -0,0 +1,22 @@
+const comments = [
+  {
+    name: "Big Deadend",
+    age: "30",
+    location: "Mesa",
+    style: "Krump",
+  },
+  {
+    name: "King Charles",
+    age: "31",
+    location: "Phoenix",
+    style: "Chicago Footwork",
+  },
+
+  {
+    name: "Armani",
+    age: "29",
+    location: "West Phoenix",
+    style: "Popping",
+  },
+];
+module.exports = comments;
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
 
+app.set("views", "views");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
